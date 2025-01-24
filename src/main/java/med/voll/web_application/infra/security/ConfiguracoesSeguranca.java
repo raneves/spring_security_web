2edfca472383f84b4703f29b20da39873556aa04
javaExactus <romulo.neves@exactus.com.br>
2025-01-24 14:17:26 -0300

    primeiro commit

---
+++ src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
@@ -0,0 +1,47 @@
+package med.voll.web_application.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration //classe de configuracao do spring e ele quem gerencia as configuracoes
+@EnableWebSecurity
+public class ConfiguracoesSeguranca {
+	@Bean 
+	public UserDetailsService dadosUsuariosCadastrados(){
+        UserDetails usuario1 = User.builder()
+                .username("joao@email.com")
+                .password("{noop}joao123")
+                .build();
+        UserDetails usuario2 = User.builder()
+                .username("maria@email.com")
+                .password("{noop}maria123")
+                .build();
+        return new InMemoryUserDetailsManager(usuario1, usuario2);
+    }
+	
+	// código omitido
+
+	@Bean
+	public SecurityFilterChain filtrosSeguranca(HttpSecurity http) throws Exception {
+		return http
+	            .authorizeHttpRequests(req -> {
+	                    req.requestMatchers("/css/**", "/js/**", "/assets/**").permitAll();
+	                    req.anyRequest().authenticated();
+	                })
+	            .formLogin(form -> form.loginPage("/login")
+	                    .defaultSuccessUrl("/")
+	                    .permitAll())
+	            .logout(logout -> logout
+	                    .logoutSuccessUrl("/login?logout")
+	                    .permitAll())
+	            .rememberMe(rememberMe -> rememberMe.key("lembrarDeMim").alwaysRemember(true))
+	            .build();
+	}
+}
